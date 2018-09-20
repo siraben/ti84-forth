d108ee010b90a4f15df9f46daea2a2029cfd6e3a
siraben <bensiraphob@gmail.com>
2018-09-21 00:01:11 +0700

    First Commit!

---
+++ README.md
@@ -0,0 +1,136 @@
+# A Forth for the TI-83+/TI-84+
+
+Hi!  This is an ongoing project to bring a Forth to the TI-83+/TI-84+
+calculator series.
+
+## Why TI-83+/TI-84+?
+This is a calculator that is more or less ubiquitous among high school
+students throughout the world.  It's not going extinct anytime soon
+(expect perhaps to newer models such as the TI-84 CE).  They're solid
+calculators, but they have notable disadvantages.  You can't really
+customize it.  There's no REPL to define procedures or perform tasks.
+Variables are limited... the list goes on.  The TI calculators are the
+microcomputers of today; so it makes sense to learn it at its lowest
+levels---starting with assembly.
+
+## Why Forth?
+Assembly is painful to program in.  Programs crash at the slightest
+hint of error, without rhyme or reason.  There's really no way to make
+an interactive REPL without creating the READ, EVAL and PRINT parts of
+it.
+
+Wouldn't it be great to have a programming language on the
+TI-83+/TI-84+ that's much faster than TI-BASIC but easier to
+understand and as low level as assembly?  Forth is just that.  (read
+_Starting FORTH_ for an excellent introduction to Forth).  It's low
+level, it's simple, but most importantly it's _easy to type_,
+especially when you're on a calculator with a non-QWERTY keyboard.  It
+is a very powerful language, allowing you to do things like change the
+syntax of the language itself.  `IF`, `WHILE`, `CONSTANT`
+etc. statements are all implemented in Forth!  Think of it as a
+untyped C with a REPL and the lower of Lisp macros.
+
+It's also easy to implement incrementally, testing as you go along.  Once
+I/O is done, the rest of Forth can be implemented in itself.
+## Requirements
+- [spasm-ng Z80 assembler](https://github.com/alberthdev/spasm-ng)
+  - If you're on a Mac you may need to run the following commands.
+  - Compile the assembler with `make` (check required packages and so
+    on).
+- A TI-84 calculator!
+  - I haven't tested this program on any emulator.
+
+```shell
+brew install openssl
+cd /usr/local/include
+ln -s ../opt/openssl/include/openssl .
+```
+
+## Building
+Once you have compiled spasm-ng, copy the executable into the same
+directly as this assembly file.  Also make sure to copy the `inc/`
+directory from the spasm-ng repository.  Once that's done, run:
+```shell
+./spasm -T forth.asm forth.8xp
+```
+The `-T` option is useful for debugging macro expansion, it's
+optional.
+
+## Features
+- 16-bit Forth on a 8-bit chip
+- Direct-threaded
+- Programs are lists of 32-bit addresses
+- Takes advantage of system calls such as `_GetKey`, `_PutC` and more
+  to come (such as drawing pixels!)
+## Example Program
+```asm
+prog:
+        .dw key, dup, dup, print_tos, space, emit, cr ;; Steps 1-6
+        .dw lit, kEnter, eql                          ;; Steps 7-8
+        .dw zbranch, -22                              ;; Step 9
+        .dw done                                      ;; Step 10
+```
+
+Here's what it does (stack shown in parentheses).
+1. Get a key from the user and place at the top of the stack. (a)
+2. Duplicate it twice. (a a a)
+3. Print the value of the top element of the stack (a a)
+4. Print a space (a a)
+5. Print the character that corresponds to the code given by the top
+   element of the stack. (a)
+6. Print a newline (a)
+7. Push the literal `kEnter` on the top of the stack (a b)
+8. Check if the two are equal (i.e. the original entered key was an
+   Enter key), and push the result on the stack (a c)
+9. If the top of the stack is zero (i.e. false), then branch back 22
+   bytes.  There are 11 instructions to be skipped but each
+   instruction takes up 2 bytes.
+10. It's that simple!
+
+### Screenshot
+![Result of running the program](demo.png)
+
+## Design Notes
+### Use of Macros
+Judicious use of macros has greatly improved readability of the code.
+This was directly inspired by the _jonesforth_ implementation (see
+Reading List).
+### Register Allocation
+One notable features of this Forth is the use of a register to keep
+track of the top element in the stack.
+
+| Z80 Register | Forth VM Register             |
+| :---:        | :---:                         |
+| DE           | Instruction pointer (IP)      |
+| HL           | Working register (W)          |
+| BC           | Top of stack (TOS)            |
+| IX           | Return stack pointer (RSP)    |
+| SP           | Parameter stack pointer (PSP) |
+### Reading List
+Documentation can vary from very well-documented to resorting to
+having to read the source code of `spasm-ng` to figure out how
+`#macro` worked.  See examples such as `defcode` and `defword`.  I
+couldn't make `defconst` or `defvar`, however, but this was fixed by
+writing it out manually.
+
+- [General Z80 guide](http://jgmalcolm.com/z80/#advanced)
+- [Moving Forth](http://www.bradrodriguez.com/papers/moving1.htm)
+- [Learn TI-83 Plus Assembly In 28 Days](http://tutorials.eeems.ca/ASMin28Days/welcome.html)
+- [KnightOS Kernel](https://github.com/KnightOS/kernel)
+- [Starting FORTH](https://www.forth.com/starting-forth/)
+
+## To be Implemented
+- [ ] Ability to read/write programs
+- [ ] Split the source into different files!
+- [ ] User input
+  - [ ] String reading routines
+  - [ ] Number reading routines
+- [ ] Output
+  - [ ] Displaying strings
+- [ ] Proper support for compile/interpret mode
+- [ ] Assembler to convert Forth words into `.dw` data segments to be
+pasted into the program.
+- [ ] Ability to switch to a "plot"
+  - [ ] So you may want to plot something and still go back to the
+        REPL when needed.
+
---
+++ forth.asm
@@ -0,0 +1,761 @@
+.NOLIST
+
+;; Doesn't care what's in HL.
+#define NEXT ld a, (de)
+#defcont   \ ld l, a
+#defcont   \ inc de
+#defcont   \ ld a, (de)
+#defcont   \ ld h, a
+#defcont   \ inc de
+#defcont   \ jp (hl)
+
+;; Verified correct.
+#define PUSH_BC_RS dec ix
+#defcont         \ ld (ix + 0), b
+#defcont         \ dec ix
+#defcont         \ ld (ix + 0), c
+
+#define POP_BC_RS  ld c, (ix + 0)
+#defcont         \ inc ix
+#defcont         \ ld b, (ix + 0)
+#defcont         \ inc ix
+
+;; Verified correct.
+#define PUSH_HL_RS dec ix
+#defcont         \ ld (ix + 0), h
+#defcont         \ dec ix
+#defcont         \ ld (ix + 0), l
+
+#define POP_HL_RS  ld l, (ix + 0)
+#defcont         \ inc ix
+#defcont         \ ld h, (ix + 0)
+#defcont         \ inc ix
+
+;; Verified correct.
+#define PUSH_DE_RS dec ix
+#defcont         \ ld (ix + 0), d
+#defcont         \ dec ix
+#defcont         \ ld (ix + 0), e
+
+#define POP_DE_RS  ld e, (ix + 0)
+#defcont         \ inc ix
+#defcont         \ ld d, (ix + 0)
+#defcont         \ inc ix
+
+#define BC_TO_HL ld h, b
+#defcont       \ ld l, c
+
+#define HL_TO_BC ld b, h
+#defcont       \ ld c, l
+
+#define HL_TO_DE ld d, h
+#defcont       \ ld e, l
+
+#define BC_TO_DE ld d, b
+#defcont       \ ld e, c
+
+;; BC = TOS   IX = RSP
+;; DE = IP    IY = UP
+;; HL = W     SP = PSP
+
+
+#include "inc/ti83plus.inc"
+
+
+start:
+        .org 9D93h
+        .db $BB,$6D
+        b_call _RunIndicOff
+        b_call _ClrLCDFull
+        b_call _HomeUp
+        
+        pop bc ;; Save the place where this program needs to go.
+        ld hl, prog_exit
+        ld (hl), c
+        inc hl
+        ld (hl), b
+        push bc
+
+        call setup_data_segment
+        ld bc, 1234
+        ld de, prog
+        NEXT
+docol:
+        PUSH_DE_RS
+        pop de
+        NEXT
+exit:
+        POP_DE_RS
+        NEXT
+done:
+        b_call _GetKey
+        b_call _ClrScrnFull
+        ld sp, (save_sp)
+        ret
+
+
+
+#macro defcode(name,len,flags,label)
+  ;; This is the first word to be defined, so set the link.
+  #ifndef prev
+    #define prev 0
+  #else
+    #define prev eval(-_)
+  #endif
+_:
+  #define label_str concat("\"",label,"\"")
+  #define name_label concat("\"name_",label,"\"")  
+  #define name_str concat("\"",name,"\"")
+  clr()  
+  wr(name_label,":")
+  wr(".dw ", eval(prev))
+  wr(".db ", eval(len+flags))
+  wr(".db ", "\"",name_str,"\",0")
+  wr(label_str, ":")
+  run()
+  
+#endmacro
+
+#define CALL_DOCOL call docol
+
+#define F_IMMED $80
+#define F_HIDDEN $20
+#define F_LEN $20
+#define F_LENMASK $1F
+
+#macro defword(name,len,flags,label)
+
+  ;; This is the first word to be defined, so set the link.
+  #ifndef prev
+    #define prev 0
+  #else
+    #define prev eval(-_)
+  #endif
+_:
+  #define label_str concat("\"",label,"\"")
+  #define name_label concat("\"name_",label,"\"")  
+  #define name_str concat("\"",name,"\"")
+  clr()  
+  wr(name_label,":")
+  wr(".dw ", eval(prev))
+  wr(".db ", eval(len+flags))
+  wr(".db ", "\"",name_str,"\",0")
+  wr(label_str, ":")
+  run()
+  CALL_DOCOL
+#endmacro
+
+        defcode("FOO",3,0,foo)
+        push bc
+        ld bc, 1234
+        NEXT
+
+        defcode("BAR",3,0,bar)
+        push bc
+        ld bc, 5678
+        NEXT
+        
+        defcode("DUP",3,0,dup)
+        push bc
+        NEXT
+
+        defcode("ADD",3,0,add)
+        pop hl
+        add hl, bc
+        HL_TO_BC
+        NEXT
+
+        defcode("SUB",3,0,sub)
+        xor a
+        pop hl
+        sbc hl, bc
+        HL_TO_BC
+        NEXT
+
+        defcode("DROP",4,0,drop)
+        pop bc
+        NEXT
+
+        defcode("SWAP",4,0,swap)
+        pop hl
+        push bc
+        HL_TO_BC
+        NEXT
+
+        defcode("OVER",4,0,over)
+        pop hl
+        push hl
+        push bc
+        HL_TO_BC
+        NEXT
+
+
+        defcode("2DROP",5,0,two_drop)
+        pop bc
+        pop bc
+        NEXT
+
+        defcode("1+", 2, 0, one_plus)
+        inc bc
+        NEXT
+
+
+        defcode("1-", 2, 0, one_minus)
+        dec bc
+        NEXT
+
+        defcode("4+", 2, 0, four_plus)
+        ld hl, 4
+        add hl, bc
+        HL_TO_BC
+        NEXT
+        
+        defcode("4-", 2, 0, four_minus)
+        ld hl, -4
+        add hl, bc
+        HL_TO_BC
+        NEXT
+
+        defcode(">R", 2, 0, to_r)
+        PUSH_BC_RS
+        pop bc
+        NEXT
+
+        defcode("R>", 2, 0, from_r)
+        POP_BC_RS
+        NEXT
+        
+        defcode("RDROP",5,0,r_drop)
+        POP_HL_RS
+        NEXT
+
+        defcode("LIT",3,0,lit)
+        ld a, (de)
+        ld l, a
+        inc de
+        ld a, (de)
+        ld h, a
+        inc de
+        push bc
+        HL_TO_BC
+        NEXT
+
+        defcode("!", 1,0,store)
+        pop hl
+        ld a, l
+        ld (bc), a
+        inc bc
+        ld a, h
+        ld (bc), a
+        NEXT
+
+        defcode("@", 1,0,fetch)
+        ld a, (bc)
+        ld l, a
+        inc bc
+        ld a, (bc)
+        ld h, a
+        HL_TO_BC
+        NEXT
+
+        defcode("+!",2,0,add_store)
+        pop hl
+        push de
+        ld a, (bc)
+        ld e, a
+        inc bc
+        ld a, (bc)
+        ld d, a
+        dec bc
+        add hl, de
+        ld a, l
+        ld (bc), a
+        inc bc
+        ld a, h
+        ld (bc), a
+        inc bc
+
+        pop de
+        NEXT
+
+        defcode("-!",2,0,sub_store)
+        pop hl
+        push de
+        ld a, (bc)
+        ld e, a
+        inc bc
+        ld a, (bc)
+        ld d, a
+        dec bc
+        xor a
+        ex de, hl
+        sbc hl, de
+
+        ld a, l
+        ld (bc), a
+        inc bc
+        ld a, h
+        ld (bc), a
+        pop de
+        NEXT
+
+        defcode("C!", 2,0, store_byte)
+        pop hl
+        ld a, l
+        ld (bc), a
+        NEXT
+        
+        defcode("C@", 2,0, fetch_byte)
+        ld a, (bc)
+        ld c, a
+        ld b, 0
+        NEXT
+
+        ;; Set the instruction pointer to the top of the stack (i.e. 
+        defcode("EXECUTE",7,0,execute)
+        BC_TO_DE
+        NEXT
+
+;; defvar("foo",3,0,foo,1000)
+;; We want that to expand to:
+;; name_foo:
+;; .dw <pointer to previous>
+;; .db 3
+;; .db "foo",0
+;; foo:
+;; push bc
+;; ld bc, var_foo
+;; NEXT
+;; var_foo:
+;; .dw 1000
+
+;; Make some variables!
+
+;; Well since there's only like 6 variables defined in Jonesforth it's
+;; not forth the effort trying to debug a macro (which took up a lot
+;; of time in this program!)
+
+#define PSP_PUSH(x) push bc \ ld bc, x
+
+#macro cell_alloc(name,initial)
+clr()
+#define name_str concat("\"",name,"\"")
+wr(name_str, ":")
+wr(".dw ", initial)
+run()
+#endmacro
+
+        ;; Actually a variable, but oh well.
+        cell_alloc(var_base,10)
+        defcode("BASE",4,0,base)
+        PSP_PUSH(var_base)
+        NEXT
+
+        ;; Are we compiling or are we interpreting?
+        cell_alloc(var_state,0)
+        defcode("STATE",5,0,state)
+        PSP_PUSH(var_state)
+        NEXT
+
+        ;; Address of the most recently defined word.
+        cell_alloc(var_latest,0)
+        defcode("LATEST",6,0,latest)
+        PSP_PUSH(var_latest)
+        NEXT
+
+        ;; Top (i.e. bottom) of the stack.
+        cell_alloc(var_sz,0)
+        defcode("S0",2,0,sz)
+        PSP_PUSH(save_sp)
+        NEXT
+
+        defcode("[",1,F_IMMED,lbrac)
+        xor a
+        ld (var_state), a
+        NEXT
+
+        defcode("]",1,F_IMMED,rbrac)
+        ld a, 1
+        ld (var_state), a
+        NEXT
+        
+save_sp:
+        .dw 0
+
+        cell_alloc(var_stack_empty,1)
+        defcode("?SE", 3, 0, stack_emptyq)
+        PSP_PUSH(var_stack_empty)
+        NEXT
+
+        cell_alloc(var_here,0)
+        defcode("HERE",4,0,here)
+        push bc
+        ld hl, (var_here)
+        ld b, h
+        ld c, l
+        NEXT
+
+        ;; Here are some constants.
+        defcode("DOCOL", 5, 0, __docol)
+        PSP_PUSH(docol)
+        NEXT
+
+        defcode("R0", 5, 0, rz)
+        PSP_PUSH(return_stack_top)
+        NEXT
+
+        defcode("F_IMMED",7,0,__F_IMMED)
+        PSP_PUSH(F_IMMED)
+        NEXT
+
+        defcode("F_HIDDEN",8,0,__F_HIDDEN)
+        PSP_PUSH(F_HIDDEN)
+        NEXT
+
+        defcode("F_LENMASK",9,0,__F_LENMASK)
+        PSP_PUSH(F_LENMASK)
+        NEXT
+
+
+        defcode("'",1,0,tick)
+        ld a, (de)
+        ld l, a   
+        inc de    
+        ld a, (de)
+        ld h, a   
+        inc de
+        push bc
+        HL_TO_BC
+        NEXT
+
+        defcode(",",1,0,comma)
+        call _comma
+        pop bc
+        NEXT
+_comma:
+        ;; Remember that var_here is a pointer, so you need to do
+        ;; double indirection!
+        push de
+        
+        ld hl, (var_here)
+
+        ld (hl), c
+        inc hl
+        ld (hl), b
+        inc hl
+        
+        ld de, var_here
+        ex de, hl
+        ld (hl), e
+        inc hl
+        ld (hl), d
+        inc hl
+
+        pop de
+
+                
+        ret
+        
+        ;; Actually, we do have a stack pointer, but it's not probably
+        ;; what is normally expected of Forths.
+        defcode("SP@", 3, 0, sp_fetch)
+        ld (var_sp), sp
+        ld hl, (var_sp)
+        push hl
+        NEXT
+
+var_sp:
+        .dw 0
+        
+        defcode("SP!", 3, 0, sp_store)
+        BC_TO_HL
+        ld sp, hl
+        NEXt
+        
+        defcode("BRANCH", 6, 0, branch)
+        ld a, (de)
+        ld l, a
+        inc de
+        ld a, (de)
+        ld h, a
+        dec de
+
+        add hl, de
+        HL_TO_DE
+        NEXT
+
+;; cpHLDE [Maths]
+;;  Compares HL to DE.
+;; Output:
+;;  Same as z80 CP instruction.
+cpHLDE:
+        or a
+        sbc hl, de
+        add hl,de
+        ret
+;; cpHLBC [Maths]
+;;  Compares HL to BC.
+;; Output:
+;;  Same as z80 CP instruction.
+cpHLBC:
+        or a
+        sbc hl, bc
+        add hl,bc
+        ret
+;; cpBCDE [Maths]
+;;  Compares DE to BC.
+;; Output:
+;;  Same as z80 CP instruction.
+cpBCDE:
+        push hl
+        ld h, b
+        ld l, c
+        or a
+        sbc hl, de
+        pop hl
+        ret
+
+
+
+        defcode("ZBRANCH", 7, 0, zbranch)
+        ld hl, 0
+        call cpHLBC
+        jp z, branch
+
+
+        ld a, (de)
+        ld l, a
+        inc de
+        ld a, (de)
+        ld h, a
+        inc de
+        
+        NEXT
+
+        defcode("?DUP",4,0,qdup)
+        ld hl, 0
+        call cpHLBC
+        jp nz, dup
+        NEXT
+
+        defcode("=", 1,0,eql)
+        pop hl
+        call cpHLBC
+        jp z, tru
+        jp fal
+
+        defcode("<>", 2,0, neql)
+        pop hl
+        call cpHLBC
+        jp z, fal
+        jp tru
+
+        defcode(">=", 1,0,greater)
+        pop hl
+        call cpHLBC
+        jp nc, tru
+        jp fal
+
+        defcode("<",1,0,less_than)
+        pop hl
+        call cpHLBC
+        jp c, tru
+        jp fal
+
+        defcode("0=",2,0,zeql)
+        ld hl,0
+        call cpHLBC
+        jp c, tru
+        jp fal
+
+;; Place a truth value on the top of the stack.
+tru:
+        ld bc, 1
+        NEXT
+        
+;; Place a false value on the top of the stack.
+fal:
+        ld bc,0
+        NEXT
+
+;; Refer to https://github.com/KnightOS/kernel/blob/830f4ac87c50fa42faf634ab3ee476f0ab85b741/src/00/strings.asm
+;; for string routines.
+
+printhl_safe:
+        push hl
+        push af
+        push de
+        b_call _DispHL
+        pop de
+        pop af
+        pop hl
+        ret
+        
+ask_msg: .db "Press a key...",0
+        defcode("ASK", 3, 0, ask)
+        ld hl, ask_msg
+        call str_println
+        NEXT
+        
+str_println:
+        b_call _PutS
+        b_call _Newline
+        ret
+        
+str_print:
+        b_call _PutS
+        ret
+
+        defcode("UPRESS", 6, 0, you_pressed)
+        ld hl, you_pressed_msg
+        call str_println
+        NEXT
+
+        defcode("KEY", 3, 0, key)
+        push bc
+        push de
+        b_call _GetKey
+        pop de
+        ld c, a
+        ld b, 0
+        NEXT
+
+        defcode("EMIT",4,0,emit)
+        ld a, c
+        b_call _PutC
+        pop bc
+        NEXT
+
+
+        defcode("U.", 2, 0, print_tos)
+        BC_TO_HL
+        call printhl_safe
+        pop bc
+        NEXT
+
+
+;; Perhaps replace the string routine with something more Forthy?
+#define BUFSIZE  48
+buffer   .EQU TextShadow
+buf_ptr  .EQU buffer + BUFSIZE + 1
+
+
+;; Inputs:
+;;  DE: Multiplier
+;;  BC: Multiplicand
+;; Outputs:
+;;  DEHL: Product of DE and BC.
+#macro mul16By16Iter
+        add hl, hl
+        rl e
+        rl d
+        jr nc, $ + 6
+        add hl, bc
+        jr nc, $ + 3
+        inc de
+#endmacro
+
+        defcode("*",1,0,mult)
+        PUSH_DE_RS
+        pop de ;; get the second element from the stack
+        ld hl, 0
+        sla e
+        rl d
+        jr nc, $ + 4
+        BC_TO_HL
+    
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        mul16By16Iter
+        
+        HL_TO_BC
+        POP_DE_RS
+        NEXT
+        
+        defword("TESTA", 5, 9, test_arith)
+        .dw dup, four_plus, print_tos
+        .dw dup, four_minus, print_tos
+        .dw dup, one_plus, print_tos
+        .dw dup, one_minus, print_tos
+        .dw print_tos, exit
+
+        defword("DOUBLE",6,0,times_two)
+        .dw dup, add, exit
+
+        defword("STAR", 4, 0, star)
+        .dw lit, 42, emit, exit
+
+        defword("STARS",5,0,stars)
+        .dw star, one_minus, zbranch, 6, branch, -10, exit
+
+        defword("SPACE",5,0,space)
+        .dw lit, 32, emit, exit
+
+        defcode("CR",2,0,cr)
+        b_call _Newline
+        NEXT
+
+        defcode("AT-XY",4,0,atxy)
+        ld a, c
+        ld (curRow), a
+        pop bc
+        ld a, c
+        ld (curCol), a
+        NEXT
+        
+
+you_pressed_msg: .db "You pressed:", 0
+
+
+quadruple_prog:
+        .dw times_two, times_two, print_tos, done
+key_count_prog:
+        .dw ask, key, you_pressed, print_tos, done
+        defword("COUNT", 5, 0, count)
+        .dw dup, print_tos, one_minus, zbranch, 6, branch, -12, exit
+
+dummy_byte: .db 0
+
+
+
+write_prog:
+        .dw lit, dummy_byte, print_tos
+        .dw lit, 10, lit, dummy_byte, store_byte, lit, dummy_byte, fetch_byte, print_tos
+        .dw done
+stars_prog:
+        .dw lit, 10, stars
+        .dw done
+
+;; Explore the mappings of keys.
+prog:
+        .dw key, dup, dup, print_tos, space, emit, cr
+        .dw lit, kEnter, eql
+        .dw zbranch, -22
+        .dw done
+        
+return_stack_top  .EQU    AppBackUpScreen+760
+
+setup_data_segment:
+        ld de, AppBackUpScreen
+        ld hl, var_here
+        ld (save_sp), sp
+        ld (hl), e
+        inc hl
+        ld (hl), d
+        ld ix, return_stack_top
+
+        ret
+
+prog_exit: .dw 0
